9addced53e3affaad84540c2f70e90c1b772785c
Niels Dommerholt <niels.dommerholt@ing.nl>
2016-10-05 09:41:10 +0200

    Nullable change

--- src/main/java/com/nibado/example/springasync/domain/ApiResponse.java
+++ src/main/java/com/nibado/example/springasync/domain/ApiResponse.java
@@ -1,13 +1,17 @@
 package com.nibado.example.springasync.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonRawValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(NON_NULL)
 public class ApiResponse {
     private String body;
     private int status;
@@ -15,6 +19,9 @@ public class ApiResponse {
 
     @JsonRawValue
     public String getBody() {
+        if(body != null && body.trim().isEmpty()) {
+            return null;
+        }
         return body;
     }
 }
